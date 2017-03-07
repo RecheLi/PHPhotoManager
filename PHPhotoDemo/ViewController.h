9c8208417d819123f276169958c810300bb91f7c
liht <liht@AppledeiMac.local>
2017-03-07 14:50:27 +0800

    Initial Commit

---
+++ PHPhotoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PHPhotoDemo
+//
+//  Created by liht on 2017/3/7.
+//  Copyright © 2017年 linitial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PHPhotoDemoTests/PHPhotoDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  PHPhotoDemoTests.m
+//  PHPhotoDemoTests
+//
+//  Created by liht on 2017/3/7.
+//  Copyright © 2017年 linitial. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PHPhotoDemoTests : XCTestCase
+
+@end
+
+@implementation PHPhotoDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
